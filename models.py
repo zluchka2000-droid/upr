897cabef2ea2853727caa96fd929a1de6c3473a0
zluchka2000-droid <zluchka2000@gmail.com>
2026-02-21 22:40:30 +0300

    models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+"""
+Database models.
+This file will be populated gradually as we add features.
+Currently just a placeholder for model imports.
+"""
+
+# We'll add models here as we implement them
+# from sqlalchemy import Column, Integer, String, ...
+# from core.database import Base
+
+# For now, this file just needs to exist so database.py can import it
+__all__ = []
